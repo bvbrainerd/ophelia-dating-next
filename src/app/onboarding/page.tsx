f1c2c8c7b968a27f677f229a0938e25b14e02f62
yao yan jiang <yaoyanjiang1@gmail.com>
2024-11-03 12:53:49 -0500

    allowing profile creation, less secure, but more efficiently coded for the sake of the MVP

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,33 +1,101 @@
-// profile set up
-// need to grab the name, gender, email
-// once it is validated and completed, you are re-directed to your dating quiz
-
-// ProfileSetup Component
 'use client';
 
 import React from 'react';
 import { useRouter } from 'next/navigation';
+import { supabase } from '../../../supabase/client';
+import { useState } from 'react';
 
 export default function Page() {
   const router = useRouter();
+  const [userInfo, setUserInfo] = useState({
+    firstName: '',
+    lastName: '', 
+    email: '',
+    age: '',
+    gender: '',
+    school: '',
+    password: '',
+  });
 
-  const handleComplete = () => {
-    // Handle form completion
-    router.push('/onboarding/dating_quiz')
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    const { name, value } = e.target;
+    setUserInfo((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
+
+  const handleComplete = async () => {
+    // Just create the profile
+    const { error } = await supabase.from('profiles').insert([
+      {
+        first_name: userInfo.firstName,
+        last_name: userInfo.lastName, 
+        email: userInfo.email,
+        age: userInfo.age,
+        gender: userInfo.gender,
+        school: userInfo.school,
+      },
+    ]);
+
+    if (error) {
+      console.error('Error creating profile:', error);
+      return;
+    }
+
+    router.push('/onboarding/dating_quiz');
+  };
+
+  // creating a profile and a user once we handle complete
 
   return (
     <div>
       <h2>Set Up Your Profile</h2>
-      <input type='text' placeholder='Full Name' />
-      <input type='number' placeholder='Age' />
-      <select>
+      {/* Added missing name attributes and fixed typo in 'email' */}
+      <input
+        type='text'
+        placeholder='First Name'
+        name='firstName'
+        value={userInfo.firstName}
+        onChange={handleChange}
+      />
+      <input
+        type='text'
+        placeholder='Last Name'
+        name='lastName'
+        value={userInfo.lastName}
+        onChange={handleChange}
+      />
+      <input
+        type='email'
+        placeholder='School Email'
+        name='email'
+        value={userInfo.email}
+        onChange={handleChange}
+      />
+      <input
+        type='password'
+        placeholder='Password'
+        name='password'
+        value={userInfo.password}
+        onChange={handleChange}
+      />
+      <input
+        type='number'
+        placeholder='Age'
+        name='age'
+        value={userInfo.age}
+        onChange={handleChange}
+      />
+      <select name='gender' value={userInfo.gender} onChange={handleChange}>
         <option value=''>Select Gender</option>
         <option value='male'>Male</option>
         <option value='female'>Female</option>
         <option value='other'>Other</option>
       </select>
-      <select>
+      <select name='school' value={userInfo.school} onChange={handleChange}>
         <option value=''>Select School</option>
         <option value='Boston College'>Boston College</option>
         <option value='Harvard'>Harvard</option>
