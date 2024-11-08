6639a9546e680b5ef08ebe7fd76e2c02af5c3d08
Natalia Kusmirek <nekusmirek@wpi.edu>
2024-11-08 14:01:41 -0500

    Fixed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import { Prompt } from 'next/font/google'
-import './globals.css'
+import { Prompt } from 'next/font/google';
+import './globals.css';
 
 const prompt = Prompt({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '700'],
-  display: 'swap',
+  subsets: ['latin'], 
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],  
+  display: 'swap', 
   variable: '--font-prompt',
-})
+});
 
 export const metadata = {
-  title: 'Ophelia Dating App',
+  title: 'Ophelia Dating',
   description: 'College Dating App',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={prompt.variable}>
       <body>{children}</body>
     </html>
-  )
+  );
 }
