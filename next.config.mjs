c2c3cf25d3b1e65699dff318a4789d52329757e9
bvbrainerd <bobrainerd@gmail.com>
2024-11-26 18:56:07 -0500

    next.mjs

---
+++ next.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL
+          ? process.env.NEXT_PUBLIC_SUPABASE_URL.replace('https://', '')
+          : 'your-default-hostname.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Ignores TypeScript errors during build
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint errors during build
+  },
+  experimental: {
+    fallbackNodePolyfills: false,
+  },
+};
+
+module.exports = nextConfig;
