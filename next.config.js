af024cd70cc86d496cd23c621d204b0fde3f1c67
bvbrainerd <bobrainerd@gmail.com>
2024-11-27 01:25:38 -0500

    nextconfiGJSON

--- next.config.js
+++ next.config.js
@@ -18,16 +18,7 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
-  },
-  typescript: {
-    ignoreBuildErrors: true, // Ignores TypeScript errors during build
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // Ignores ESLint errors during build
-  },
-  experimental: {
-    fallbackNodePolyfills: false,
-  },
+  }
 };
 
 module.exports = nextConfig;
