e9a0bedeb1189b6913867cb5522d22a2d228a8c2
bvbrainerd <bobrainerd@gmail.com>
2024-11-27 13:10:35 -0500

    payment pages

---
+++ next.config.js
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL
+          ? process.env.NEXT_PUBLIC_SUPABASE_URL.replace('https://', '')
+          : 'your-default-hostname.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    skipTypechecking: true,
+    forceSwcTransforms: true,
+  }
+};
+
+module.exports = nextConfig;
