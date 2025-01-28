d16fbfcae837bf44222944831d7d90f2f78c03d9
bvbrainerd <bobrainerd@gmail.com>
2025-01-28 01:53:11 -0500

    sendgrid

--- src/app/send-valentine/page.tsx
+++ src/app/send-valentine/page.tsx
@@ -68,7 +68,7 @@ export default function SendValentinePage() {
         dynamicTemplateData: {
           senderName: isAnonymous ? 'Someone special' : `${senderProfile?.first_name} ${senderProfile?.last_name}`,
           recipientName: recipientName,
-          valentineLink: `${process.env.NEXT_PUBLIC_APP_URL || window.location.origin}/valentine/${newRequest.id}`
+          valentineLink: `https://opheliadatingapp.com/valentine/${newRequest.id}`
         }
       };
 
