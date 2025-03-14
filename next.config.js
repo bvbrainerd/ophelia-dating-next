af48538dde6cc3f198c20dddbc2ccf48ca8a6519
bvbrainerd <bobrainerd@gmail.com>
2025-03-13 22:46:46 -0400

    json

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     domains: ['oyjfhrqfufujmsnqevgr.supabase.co'],
-    unoptimized: true,
+  },
+  // Disable automatic static optimization for API routes
+  experimental: {
+    serverActions: true,
   },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
