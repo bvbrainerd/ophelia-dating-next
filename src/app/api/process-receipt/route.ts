2c445fb5416d23aa95a7b0d5bdfe33501c14e31d
Daniel Jun <dhj190001@utdallas.edu>
2025-03-14 16:23:03 -0400

    created api endpoint for processing uploaded receipts and extracting data

---
+++ src/app/api/process-receipt/route.ts
@@ -0,0 +1,80 @@
+import { NextResponse } from 'next/server';
+const DocumentIntelligence = require("@azure-rest/ai-document-intelligence").default,
+{ getLongRunningPoller, isUnexpected } = require("@azure-rest/ai-document-intelligence");
+import { Receipt } from '@/types/receipt';
+
+// const receiptUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/rest-api/receipt.png";
+
+const endpoint = process.env.AZURE_FORM_RECOGNIZER_URL;
+const key = process.env.AZURE_FORM_RECOGNIZER_KEY;
+
+export async function POST(request: Request) {
+  try {
+    const data = await request.formData();
+    const file = data.get('file') as File | null;
+    if (!file) return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
+
+    // Convert the file to a base64 string
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const base64image = buffer.toString("base64");
+
+    console.log('base64image:', base64image);
+
+    // create client instance and run analysis
+    const client = DocumentIntelligence(endpoint, {key:key});
+    const initialResponse = await client
+      .path("/documentModels/{modelId}:analyze", "prebuilt-receipt")
+      .post({
+          contentType: "application/json",
+          body: {
+              base64Source: base64image,
+          },
+      });
+
+      if (isUnexpected(initialResponse)) {
+        throw initialResponse.body.error;
+      }
+
+
+      // start polling for results
+      const poller = getLongRunningPoller(client, initialResponse);
+      const analyzeResult = (await poller.pollUntilDone()).body.analyzeResult;
+
+      const documents = analyzeResult?.documents;
+      const result = documents && documents[0];
+  
+    if (result) {
+      const receipt: Receipt = {
+        merchant: result.fields.MerchantName?.valueString || "Unknown",
+        date: result.fields.TransactionDate?.valueDate || new Date().toISOString(),
+        subtotal: result.fields.Subtotal?.valueCurrency?.amount || 0,
+        total: result.fields.Total?.valueCurrency?.amount || 0,
+        tax: result.fields.Tax?.valueCurrency?.amount || 0,
+        tip: result.fields.Tip?.valueCurrency?.amount,
+        currency: result.fields.Total?.valueCurrency?.currencyCode || "USD",
+        items: (result.fields.Items?.valueArray || []).map(( item: any ) => {
+
+          return {
+            name: item.valueObject?.Description?.valueString ?? "Unknown Item",
+            quantity: item.valueObject?.Quantity?.valueNumber ?? 1,
+            totalPrice: item.valueObject?.TotalPrice?.valueCurrency?.amount ?? 0,
+          };
+        }),
+      };
+      
+      // console.log('Receipt:', formattedReceipt);
+      return NextResponse.json({ receipt: receipt });
+    } else {
+      return NextResponse.json(
+        { error: 'Error processing receipt' },
+        { status: 500 }
+      );    
+    }
+  } catch (error) {
+    console.error('Error processing receipt:', error);
+    return NextResponse.json(
+      { error: 'Error processing receipt' },
+      { status: 500 }
+    );
+  }
+}
