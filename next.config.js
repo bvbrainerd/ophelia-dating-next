2b148f3c3509b4609618b756776e3e67ef393bef
bvbrainerd <bobrainerd@gmail.com>
2025-03-31 11:23:38 -0400

    deployment errors

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  // Remove the experimental.serverActions line as it's now default
   images: {
-    // domains: [
-    //   'oyjfhrqfufujmsnqevgr.supabase.co',
-    //   'localhost',
-    //   'supabase.co'
-    // ],
     remotePatterns: [
       {
         protocol: 'https',
@@ -15,8 +11,18 @@ const nextConfig = {
         pathname: '/storage/v1/object/public/**',
       },
     ],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        path: false, // Add this line
+      };
+    }
+    return config;
   },
 }
 
