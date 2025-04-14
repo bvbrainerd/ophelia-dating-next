5c8ec961c2b84744c1ab5dfc88ad40795906e98d
Daniel Jun <dhj190001@utdallas.edu>
2025-04-14 17:51:36 -0400

    Updated mailgun route to use mailgun's inbound parse format

--- src/app/api/webhooks/sendgrid/inbound/route.ts
+++ src/app/api/webhooks/sendgrid/inbound/route.ts
@@ -3,40 +3,19 @@ import { createClient } from "redis";
 
 export async function POST(request: Request) {
   try {
-    const client = createClient();
-    client.on("error", (err) => console.error("Redis Client Error", err));
-    await client.connect();
 
     const rawBody = await request.formData(); // SendGrid sends raw text or form data
-    // console.log('rawBody', rawBody);
-    const envelope = JSON.parse(rawBody.get("envelope") as string);
-    const emailAddress = envelope.to[0];
-    const uniqueId = emailAddress.split("+")[1].split("@")[0];
-    const emailBody = rawBody.get('email')?.toString();
+    console.log('rawBody', rawBody);
 
-    const verificationCodeRegex = /Your verification code is (\w+)/i;
-
-    const match = emailBody?.match(verificationCodeRegex);
-    const verificationCode = match ? match[1] : null;
-
-    
-    // console.log("verification code", verificationCode);
-    if (uniqueId === undefined) {
-      console.log("uniqueId is undefined");
-    }
-    else if (verificationCode === null) {
-      console.log("verificationCode is null");
+    const recipient = rawBody.get("recipient") as string;
+    const emailType = recipient.split("+")[0];
+    if (emailType !== "reserve") {
+      console.log("Email type is not reserve");
+      return NextResponse.json({ status: "error" }, { status: 400 });
     }
     else {
-      await client.set('verification_code_' + uniqueId, verificationCode, {
-        EX: 60 * 5, // Set expiration time to 5 minutes
-      });
-      await client.get('verification_code_' + uniqueId);
-      console.log("verification code set in redis", verificationCode);
+      opentableVerificationEmail(rawBody);
     }
-
-    await client.quit();
-
 
     return NextResponse.json({ status: "success" }, { status: 200 });
   } catch (error) {
@@ -44,3 +23,48 @@ export async function POST(request: Request) {
     return NextResponse.json({ status: "error" }, { status: 500 });
   }
 }
+
+const opentableVerificationEmail = async (rawBody: any) => {
+  const client = createClient();
+  try {
+      client.on("error", (err) => console.error("Redis Client Error", err));
+      await client.connect();
+      const recipient = rawBody.get("recipient") as string;
+      const sender = rawBody.get("sender") as string;
+      const strippedText = rawBody.get("stripped-text") as string;
+      console.log("recipient", recipient);
+      console.log("sender", sender);
+      console.log("strippedText", strippedText);
+      // const envelope = JSON.parse(rawBody.get("envelope") as string);
+      // const emailAddress = envelope.to[0];
+      const uniqueId = recipient.split("+")[1].split("@")[0];
+      // const emailBody = rawBody.get('email')?.toString();
+
+      const verificationCodeRegex = /Your verification code is (\w+)/i;
+
+      const match = strippedText?.match(verificationCodeRegex);
+      const verificationCode = match ? match[1] : null;
+
+      
+      // console.log("verification code", verificationCode);
+      if (uniqueId === undefined) {
+        console.log("uniqueId is undefined");
+      }
+      else if (verificationCode === null) {
+        console.log("verificationCode is null");
+      }
+      else {
+        // await client.set('verification_code_' + uniqueId, verificationCode, {
+        //   EX: 60 * 5, // Set expiration time to 5 minutes
+        // });
+        // await client.get('verification_code_' + uniqueId);
+        console.log("verification code set in redis", verificationCode);
+      }
+
+      // await client.quit();
+    } catch (error) {
+      console.error("Error:", error);
+      await client.quit();
+      return NextResponse.json({ status: "error" }, { status: 500 });
+    }
+  }
